dbb286a589109896c42f5479cc92034e1ae2c232
SubbirChowdhury <subbirchowdhury27@gmail.com>
2025-07-20 02:07:21 +0600

    Basic Numercal Operations

---
+++ Basic Numerical Operations.py
@@ -0,0 +1,20 @@
+# Addition of two number
+print(11+4)
+
+# Subtraction between two number.
+print(11-4)
+
+# Multiple of two numbers.
+print(11*4)
+
+# Divided of two numbers.
+print(11/4)
+
+# Vagshesh ber korar jonno
+print(11%4)
+
+# Vagfol Ber korar jonno.
+print(11//4)
+
+# Exponinsial or to-the-power
+print(2**3)  #2 to-the-power 3
