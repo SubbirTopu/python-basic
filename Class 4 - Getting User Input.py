6b8c7dc90fb60d17304c33b1f42337a5b2663b32
SubbirChowdhury <subbirchowdhury27@gmail.com>
2025-07-20 02:30:48 +0600

    Getting User Input

---
+++ Class 4 - Getting User Input.py
@@ -0,0 +1,11 @@
+name = input("Enter Your Name : ")
+age = input("Enter your Age : ")
+GPA = input("Enter Your GPA : ")
+
+
+
+print("Student Information")
+print("-------------------")
+print(f"Name : {name}")
+print(f"Age : {age}")
+print(f"GPA : {GPA}")
