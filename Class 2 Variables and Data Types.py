623135cdaf7ca906b6cf5987d9901117abe15f4f
SubbirChowdhury <subbirchowdhury27@gmail.com>
2025-07-20 01:48:06 +0600

    Variables and Data type

--- Class 2 Variables and Data Types.py
+++ Class 2 Variables and Data Types.py
@@ -1,5 +1,5 @@
 # Declare Variable
-'''
+
 name = "Amlan"
 age = 30
 cgpa = 3.98
@@ -9,7 +9,7 @@ print(name + "lives in Dhaka")
 print("He is currently", age, "years old")
 print("At the age of", age, "he has started to learn python")
 print("Amlan has scored", cgpa, "in his bachelor degree ")
-'''
+
 
 
 
@@ -27,3 +27,30 @@ print(name + " has scored", cgpa, "in his bachelor degree.")
 print(f"My name is: {name}")
 print(f"My age is:{age}")
 print(f"My mane is: {name} and my brother name is:{name}")
+
+# We can write it easy way
+
+name = "Murad"
+age = 26
+cgpa = 3.12
+
+print(f"Our New student name is {name}")
+print(f"{name} lives in Dhaka.")
+print(f"{name} is currently {age} years old.")
+print(f"At the age of {age}, {name} started to learn python.")
+print(f"{name} has scored {cgpa} in his bachelor degree")
+print(f"{name} never gives up, {name} faces many obstacles. However {name} didn't give up")
+print(f"{name} can do anything")
+
+
+
+
+print("Our new student name is Topu")
+print("Topu lives in Dhaka")
+print("He is currently 30 years old")
+print("At the age of 30 he has started to learn python")
+print("Topu has scored 3.16 in his bachelor degree ")
+print("Topu beleive that he can do anything")
+print("Last 2 years he have more capability but he can't")
+
+
