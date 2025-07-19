4840da9681d2a0da88134000183e1b5a5c9af842
SubbirChowdhury <subbirchowdhury27@gmail.com>
2025-07-19 12:12:24 +0600

    variables practice

---
+++ Class 1.py
@@ -0,0 +1,21 @@
+# My Biodata (single Line Comment)
+'''
+Multiple
+Line
+comment
+'''
+
+print("Sabbir Chowdhury")
+print("01629334432")
+
+# Backslash Character (New Line)
+print("Mehedi Hasan \n 01980093983")
+
+# Backslash Character (Tab)
+print("Fardin Hasan \t 01760391996")
+
+# Backslash Character (Doble Quotation)
+print("\"Fetima Asha\"")
+
+# Backslash Character (Single Quotation)
+print('\'Niloy\'')
---
+++ Class 2 Variables and Data Types.py
@@ -0,0 +1,25 @@
+# Declare Variable
+'''
+name = "Amlan"
+age = 30
+cgpa = 3.98
+print("Our new student name is" + name)
+print(name + "lives in Dhaka")
+# For Integer should be use ,  ,
+print("He is currently", age, "years old")
+print("At the age of", age, "he has started to learn python")
+print("Amlan has scored", cgpa, "in his bachelor degree ")
+'''
+
+
+
+name = "Mahmood"
+age = 40
+cgpa = 3.50
+
+
+print("Our New student name is " + name)
+print(name + " lives in Dhaka.")
+print("He is currently", age, "years old.")
+print("At the age of", age,  name + " started to learn python.")
+print(name + " has scored", cgpa, "in his bachelor degree.")
