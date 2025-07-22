83e5059a561f04ba6c7925f6840f26dc672402cf
SubbirChowdhury <subbirchowdhury27@gmail.com>
2025-07-23 02:12:32 +0600

    Area of any shape

---
+++ Class 6 - Area of any shape.py
@@ -0,0 +1,18 @@
+# Triangle area calculate
+'''
+base = float(input("Enter Base = "))
+height = float(input("Enter Height = "))
+
+area = 0.5 * base * height
+
+print(area)
+'''
+# Circle area calculate
+
+radius = float(input("Enter Radius = "))
+
+area = 3.1416 * radius * radius
+
+print(area)
+
+
